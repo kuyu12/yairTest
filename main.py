6bb25d6e324e33cc2b0f97681243838efad9339e
Yair Hadad <Yair_hadad@epam.com>
2024-07-10 23:24:58 +0300

    V1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hi')
+import google.generativeai as genai
+import urllib3
+
+genai.configure(api_key='AIzaSyDaENsGTWKEJU4IdEkUW7dUezsDXHgxrDc')
+model = genai.GenerativeModel('gemini-1.5-flash')
+response = model.generate_content("What is the meaning of life?")
+
+print(response)
+
+
+print("this is america")
